f74d7c7479b63f70ac4bd03149f93d9c284063df
Jonas Donkor <donkoronas00@gmail.com>
2024-01-20 18:43:46 -0800

    made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Services from './components/Services';
