f4af21fbe2d91324b665eb3414cd7c256453620d
Jonas Donkor <donkoronas00@gmail.com>
2024-01-20 16:16:41 -0800

    made some changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import service2 from '../assets/service2.jpg';
+import Banner4 from '../assets/Banner4.jpg';
 
 function about() {
     
@@ -10,10 +10,10 @@ function about() {
             <div className='w-full px-4 mb-10 lg:w-1/2 lg:mb-0'>
               <div className='relative lg:max-w-md'>
                 <img className='relative z-10 object-cover w-full rounded h-full'
-                src={service2} alt="aboutimage" />
+                src={Banner4} alt="aboutimage" />
                 <div className='absolute bottom-0 right-0 z-10 p-8 bg-white border-4 border-orange-400 rounded shadow lg:-mb-8 lg:-mr-11 sm:p-8'>
-                  <p className='text-lg font-semibold md:w-72'>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="orange" className='absolute top-0 left-0 w-16 h-16 text-orange-400 opacity-10'
+                  <p className='text-md font-semibold md:w-72'>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="orange" className='absolute top-0 left-0 w-10 h-10 text-orange-400 opacity-10'
                                     viewBox="0 0 16 16">
                                     <path
                                         d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -38,7 +38,7 @@ function Navbar() {
 
    
   return (
-    <header className='w-full, bg-white md:bg-transparent fixed top-0 left-0 right-0 border-b shadow-sm'>
+    <header className='w-full, bg-white fixed top-0 left-0 right-0 border-b shadow-sm'>
         <nav className={`py-4 lg:px-14 px-4 ${isSticky ? "sticky top-0 left-0 right-0 border-b bg-white duration-300" :""}`}>
             <div className='flex justify-between items-center text-base gap-8'>
                 <a href='' className='text-2xl font-semibold flex items-center space-x-3'><img src={logo} alt='' className='w-10 inline-block items-center'/><span className='text-orange-500'>RichwayElectricals</span></a>
